732ac3c98ed4911f64463cae2b8e4204ac83e4d3
Mike <snowlove@users.noreply.github.com>
2020-04-29 19:41:12 -0500

    Initial commit

---
+++ IncisionDKP/RetrieveDKP.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace IncisionDKP
+{
+    class RetrieveDKP
+    {
+        private const string pathFile = @"D:\World of Warcraft\_classic_\WTF\Account\MIKE91\SavedVariables\MonolithDKP.lua";
+        private static string MonoDKP { get; set; }
+
+        public class Players
+        {
+            public string playerName { get; set; }
+            public int totalDKP { get; set; }
+        }
+
+        public RetrieveDKP()
+        {
+            LoadFileContents();
+        }
+
+        private static void LoadFileContents()
+        {
+            if (!File.Exists(pathFile))
+                throw new Exception("Could not find the Monolith DKP File.\r\nLocation: " + pathFile);
+
+            string[] c = File.ReadAllLines(pathFile);
+            int index = 0;
+            bool logging = false;
+
+            foreach(string line in c)
+            {
+                if (line.Contains("MonDKP_DKPTable"))
+                    logging = true;
+                else if (line.Contains("MonDKP_DKPHistory"))
+                    logging = false;
+
+                index++;
+
+                if (!logging)
+                    continue;
+
+
+            }
+        }
+
+        private static void moo()
+        {
+            File.ReadAllLines("");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# IncisionDKP Desktop Application
+
+To integrate MonolithDKP current values, bidding on items, etc to discord.
+
+TODO: 
+* Need to find Discord API for C#
+* Read values from Monolith SavedVariables.
+* Write AddOn to track items during raid to dump to SavedVariables (/reloadui, to trigger flush out) and then post them on discord for bidding via Direct Message.
